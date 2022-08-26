5c0642fbfbea386a4f82888ff1bc5818f14c4797
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-08-26 11:07:22 +0100

    Files and folders structure, @mui/material

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './assets/components/Navbar';
+import Footer from './assets/components/Footer';
+import Home from './assets/pages/Home';
+import ExerciseDetail from './assets/pages/ExerciseDetail';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
      <h1>App</h1>
-    </div>
+     <Navbar />
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/exercise/:id' element={<ExerciseDetail />} />
+     </Routes>
+     <Footer />
+    </Box>
   );
 }
 
